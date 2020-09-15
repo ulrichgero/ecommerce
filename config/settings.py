e270da8f0c6e7057c14a42558cc28757bb03fcf6
ulrichgero <ulrichgero@gmail.com>
2020-09-15 15:51:40 +0200

    settigns file update

--- config/settings.py
+++ config/settings.py
@@ -87,9 +87,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ulrichgero@gmail.com'
-EMAIL_HOST_PASSWORD = 'qszatlrvfimywfod'
-DEFAULT_FROM_EMAIL = 'Password Reset From Kareeba'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = 'Password Reset From '
 #EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 
 
